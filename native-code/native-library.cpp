e7dd0e97070eda08108c2e198e836d366fcb3026
shashibyadav <shashi.shuttle2@gmail.com>
2024-05-16 20:34:49 -0400

    remomved comments

--- native-code/native-library.cpp
+++ native-code/native-library.cpp
@@ -12,11 +12,3 @@ JNIEXPORT jint JNICALL Java_objects_NativeSum_addNumbers (JNIEnv *env, jclass ob
     // Return the result (converted to jint)
     return sum;
 }
-
-//JNIEXPORT jint JNICALL Java_objects_NativeSum_addNumbers(JNIEnv *env, jobject obj, jint a, jint b) {
-    // Perform data processing (addition)
-//    int sum = a + b;
- //   std::cout << "Inside native code" << std::endl;
-    // Return the result (converted to jint)
- //   return sum;
-//}
